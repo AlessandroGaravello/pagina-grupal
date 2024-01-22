e7456bd1db33ff5ee4b44cf7eb7938c3534e0460
Natalia Ramírez Benito <117905300+Natalia-Ramirez-Benito@users.noreply.github.com>
2024-01-22 11:28:14 +0100

    Tab3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Platform} from "@ionic/angular";
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,51 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  // Define las habilidades y lenguajes aquí
+  languages = [
+    { name: 'JetBrains', icon: 'jetbrains' },
+    { name: 'Java', icon: 'java' },
+    { name: 'Kotlin', icon: 'kotlin' },
+    { name: 'Firebase', icon: 'firebase' },
+    { name: 'Android Studio', icon: 'android-studio' },
+    { name: 'PHP', icon: 'php' },
+    { name: 'Tomcat', icon: 'tomcat' },
+    { name: 'CSS', icon: 'css' },
+    { name: 'Bootstrap', icon: 'bootstrap' },
+    { name: 'Tailwind', icon: 'tailwind' },
+    { name: 'Angular', icon: 'angular' },
+    { name: 'Ionic', icon: 'ionic' },
+    { name: 'HTML', icon: 'html' },
+    { name: 'JavaScript', icon: 'javascript' },
+    { name: 'MySQL', icon: 'mysql' },
+    { name: 'PostgreSQL', icon: 'postgresql' },
+    { name: 'MongoDB', icon: 'mongodb' },
+    { name: 'SQLite', icon: 'sqlite' },
+    { name: 'Git', icon: 'git' },
+    { name: 'Python', icon: 'python' },
+    { name: 'Anaconda', icon: 'anaconda' },
+    { name: 'Unity', icon: 'unity' },
+    { name: 'C#', icon: 'c-sharp' },
+  ];
 
+  // Define los idiomas aquí
+  languagesProficiency = [
+    { language: 'Español', flag: 'es', proficiency: 'Nativo' },
+    { language: 'Inglés', flag: 'us', proficiency: 'Avanzado' },
+    // Agrega otros idiomas aquí
+  ];
+
+  // Agrega más información de contacto si es necesario
+  contactEmail = 'tucorreo@example.com';
+
+  constructor(private platform: Platform) {}
+
+  sendEmail() {
+    if (this.platform.is('android') || this.platform.is('ios')) {
+      window.open(`mailto:${this.contactEmail}`, '_system');
+    } else {
+      // Lógica para manejar otros dispositivos o navegadores
+      // Puede ser una redirección a una página de contacto, por ejemplo.
+    }
+  }
 }
